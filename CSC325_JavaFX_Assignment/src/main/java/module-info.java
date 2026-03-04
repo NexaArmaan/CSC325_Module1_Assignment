95d77f7874bdfaf809d22c4011703b7351f8fb79
Armaan Arora <armaanarora880@gmail.com>
2026-03-04 12:37:18 -0500

    removed misplaced project

--- CSC325_JavaFX_Assignment/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.csc325_javafx_assignment {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.csc325_javafx_assignment to javafx.fxml;
-    exports org.example.csc325_javafx_assignment;
-}
